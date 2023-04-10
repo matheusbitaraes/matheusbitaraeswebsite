8c2d3caf1d424ad04ab540f2232d3935b03ce4a9
matheus <matheusbitaraesdenovaes@gmail.com>
2023-04-10 19:15:10 +0100

    add lighter background

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -29,9 +29,7 @@ class Portfolio extends Component {
       <div className="row">
 
          <div className="twelve columns collapsed">
-
             <h1>Check Out Some of My Works.</h1>
-
             <div id="portfolio-wrapper" className="bgrid-quarters s-bgrid-thirds cf">
                 {projects}
             </div>
