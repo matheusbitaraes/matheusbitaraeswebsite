05dffecb2a7753366bc710ab53b5f73394451c03
matheus <matheusbitaraesdenovaes@gmail.com>
2023-04-10 20:43:42 +0100

    improve bio

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,13 +4,11 @@ class About extends Component {
   render() {
 
     if(this.props.data){
-      var name = this.props.data.name;
       var profilepic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
       var street = this.props.data.address.street;
       var city = this.props.data.address.city;
       var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
       var phone= this.props.data.phone;
       var email = this.props.data.email;
       var resumeDownload = this.props.data.resumedownload;
@@ -30,9 +28,9 @@ class About extends Component {
                <div className="columns contact-details">
                   <h2>Contact Details</h2>
                   <p className="address">
-						   <span>{name}</span><br />
+						   <span>Matheus Bitarães de Novaes</span><br />
 						   <span>{street}<br />
-						         {city} {state}, {zip}
+						         {city}, {state}.
                    </span><br />
 						   <span>{phone}</span><br />
                      <span>{email}</span>
