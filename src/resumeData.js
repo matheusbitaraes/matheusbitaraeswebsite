288ba1e046775edce1071a2cf7c5b87662191dbd
Matheus <matheusbitaraesdenovaes@gmail.com>
2024-03-17 15:21:07 -0300

    update design

---
+++ src/resumeData.js
@@ -0,0 +1,221 @@
+const resumeData = {
+    "main": {
+        "name": "Matheus",
+        "occupation": "Software Engineer, MSc in Computational Intelligence",
+        "description": "passionate about music and solving problems",
+        "image": "profile.JPG",
+        "bio": "I'm a deadline-oriented Systems Engineer with experience working on global companies and dealing with coworkers from different countries. <br> My tech stack includes mainly <b>Java, Python, Javascript, Node.js, Vue.js, React, Docker, Kubernetes, AWS, Google Cloud, PostgreSQL, and Mongodb </b>. <br> I hold a Masters in Engineering (Computational Intelligence). My research focused on Algorithmic Music Generation. My thesis explored using Genetic Algorithms and Generative Adversarial Networks (GANs) for creating melodies with specific harmonic functions. Additionally, I published an article on the use of data augmentation strategies to improve the quality of GAN-based musical melody generators.",
+        "contactmessage": "Here is where you should write your message to readers to have them get in contact with you.",
+        "email": "matheusbitaraesdenovaes@gmail.com",
+        "phone": "+55 31 99485-1781",
+        "address": {
+            "city": "Belo Horizonte",
+            "state": "Minas Gerais"
+        },
+        "website": "http://www.bitaraes.com.br",
+        "resumedownload": "documents/matheus_resume.pdf",
+        "social": [
+            {
+                "name": "linkedin",
+                "url": "https://www.linkedin.com/in/matheus-bitaraes/",
+                "className": "fa fa-linkedin"
+            },
+            {
+                "name": "instagram",
+                "url": "https://instagram.com/matheusbitaraes",
+                "className": "fa fa-instagram"
+            },
+            {
+                "name": "github",
+                "url": "https://github.com/matheusbitaraes/",
+                "className": "fa fa-github"
+            }
+        ]
+    },
+    "resume": {
+        "skillmessage": "",
+        "education": [
+            {
+                "school": "UFMG - Universidade Federal de Minas Gerais (Brazil)",
+                "degree": "Masters in Computational Intelligence",
+                "graduated": "2023",
+                "description": "Masters in Engineering (Computational Intelligence). Research field: <b>Algorithmic Music Generation.</b><br/>My thesis explored the use of Genetic Algorithms and Generative Adversarial Networks (GANs) for creating melodies with specific harmonic functions, using a weighted sum of heuristic methods to evaluate quality.\n In addition, I have published an article on the use of data augmentation strategies to improve the quality of GAN-based musical melody generators. Through my research, I have developed expertise in artificial intelligence, machine learning, and music theory.\nOverall, my studies have allowed me to combine my passion for music with my interest in technology and AI, leading me to explore new avenues in the emerging field of algorithmic art."
+            },
+            {
+                "school": "UFMG - Universidade Federal de Minas Gerais (Brazil)",
+                "degree": "Systems Engineering Bachelor Degree",
+                "graduated": "2018",
+                "description": "Final Thesis presented and achieved a grade of 95/100. It was about making a drum using sound and image processing."
+            },
+            {
+                "school": "QUT - Queensland University of Technology (Australia)",
+                "degree": "Interchange Engineering Program",
+                "graduated": "2015",
+                "description": "10 months studying Engineering in Australia by program Science Without Borders. Learned programming languages such as python, c# e SQL. Role of Project Manager in a team with 8 students with the objective of developing a UAV (unmanned aerial vehicle) using Systems Engineering and project managing methods. At the end of the project, the UAV could perform with 90% of the objectives with success. Demonstration video available <a href='https://youtu.be/mdGK8ZmwnYk' target='_blank' >here</a>."
+            },
+            {
+                "school": "CEFET MG - Centro Federal de Educação Tecnológica (Brazil)",
+                "degree": "High School and Technician",
+                "graduated": "2010",
+                "description": "Mechatronics Technician."
+            }
+        ],
+        "work": [
+            {
+                "company": "Draft (fka Contentfly)",
+                "title": "Software Engineer",
+                "years": "2022 - 2023",
+                "description": "Worked building an AI content generation platform using GPT networks for various types of content such as blog posts and product descriptions.",
+                "technologies": [
+                    "Vue.js",
+                    "Javascript",
+                    "Node.js",
+                    "MongoDB",
+                    "PostgreSQL"
+                ]
+            },
+            {
+                "company": "Americanas SA",
+                "title": "Back End Software Engineer",
+                "years": "2021 - present",
+                "description": "Responsible for implementing solutions for enhancing user experience in Americanas' search engine. Americanas S.A is one of the biggest retail companies in South America.",
+                "technologies": [
+                    "Java",
+                    "Springboot",
+                    "Python",
+                    "Kubernetes",
+                    "Docker",
+                    "Terraform",
+                    "Solr",
+                    "Gitlab",
+                    "AWS Cloud",
+                    "Google Cloud"
+                ]
+            },
+            {
+                "company": "Omnilogic SA",
+                "title": "Full Stack Developer - Squad Lead",
+                "years": "2019 - 2021",
+                "description": "Responsible for technically leading a squad aimed to maintain and evolve company products",
+                "technologies": [
+                    "Java",
+                    "Javascript",
+                    "React",
+                    "Graphql",
+                    "Elasticsearch",
+                    "Docker",
+                    "Kubernets",
+                    "Scala",
+                    "SQL",
+                    "Mongodb"
+                ]
+            },
+            {
+                "company": "FCA - Fiat Chrysler Automobiles",
+                "title": "Global Innovation Analyst",
+                "years": "2018 - 2019",
+                "description": "Approval on concurred internal process. Responsible for researching, planning, developing, constructing prototypes and Proof-of-Concepts of innovative products according the company global innovation plan.",
+                "technologies": ["Python", "Matlab", "Simulink"]
+            },
+            {
+                "company": "FCA - Fiat Chrysler Automobiles",
+                "title": "Mopar Junior Analyst",
+                "years": "2016 - 2018",
+                "description": "Participation on global integration projects and global vehicles developments (on spare parts infrastructure standpoint).",
+                "technologies": []
+            },
+            {
+                "company": "ENACOM",
+                "title": "Engineering Internship",
+                "years": "2016 - 2016",
+                "description": "Internship working with Matlab implementations in the field of game theory and optimization",
+                "technologies": ["Matlab"]
+            },
+            {
+                "company": "ARCAA - Australian Research Centre for Aerospace Automation (Australia)",
+                "title": "Engineering Work Experience",
+                "years": "2015 - 2015",
+                "description": " - Thermal cameras fine-tuning for health analysis on crops. \n - Research and development of algorithms (in Matlab) for recognition of wildlife in thermal imaging.",
+                "technologies": []
+            },
+            {
+                "company": "Fiat Automóveis",
+                "title": "Junior Analyst",
+                "years": "2012 - 2014",
+                "description": " - Participation on global projects for FIAT - CHRYSLER integration. \n - Responsible for the Spare Parts Catalog of models Fiat 500 and Freemont \n - Responsible for the Spare Parts Catalog regarding exhaust, cold start and air filter of all FIAT vehicles."
+            },
+            {
+                "company": "Metalfisa",
+                "title": "Technician Internship",
+                "years": "2011 - 2012",
+                "description": " - Project development and maintenance."
+            }
+        ],
+        "skills": [
+            {
+                "name": "Git",
+                "level": "95%"
+            },
+            {
+                "name": "Java",
+                "level": "70%"
+            },
+            {
+                "name": "React",
+                "level": "60%"
+            },
+            {
+                "name": "CSS",
+                "level": "90%"
+            },
+            {
+                "name": "HTML5",
+                "level": "80%"
+            },
+            {
+                "name": "VueJs",
+                "level": "50%"
+            },
+            {
+                "name": "MongoDB",
+                "level": "80%"
+            }
+        ]
+    },
+    "portfolio": {
+        "projects": [
+            {
+                "title": "AI Hero",
+                "category": "An algorithmic music combosition system",
+                "image": "aihero.png",
+                "url": "https://aihero.bitaraes.com.br"
+            },
+            {
+                "title": "Southland Merchands Website",
+                "category": "Developed with Gatsby and with integration with Medium.",
+                "image": "southlandmerchants.png",
+                "url": "https://www.southlandmerchants.com.au/"
+            },
+            {
+                "title": "Ana Detomi Website",
+                "category": "Developed with NextJs",
+                "image": "anadetomi.png",
+                "url": "https://anadetomi.com.br/"
+            }
+        ]
+    },
+    "testimonials": {
+        "testimonials": [
+            {
+                "text": "Here you should write some nice things that someone has said about you. No lies though, employers can tell when you are lying.",
+                "user": "Kareem Abdul Jabbar"
+            },
+            {
+                "text": "That Tim Baker must be one of the most brilliant developers I've ever met! It is amazing that nobody has hired him yet. Hey you, you should hire this guy, he may be fresh out of University and have zero on the job experience but I am confident that he will be one of your best developers in no time!",
+                "user": "Steve Wozniak... impersonator"
+            }
+        ]
+    }
+}
+
+export default resumeData
